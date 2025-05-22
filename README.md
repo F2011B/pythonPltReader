61a8385b9259b05498546263ebb8224500a6d1a9
Dark Light <F2011B@users.noreply.github.com>
2025-05-22 23:16:16 +0200

    Add packaging

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Should deliver a interface for a tecplot binary file for python.
+Simple interface for a Tecplot binary file for Python.
+
+## Installation
+
+Install from source using pip:
+
+```bash
+pip install .
+```
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tecplotPltReader',
+    version='0.1.0',
+    py_modules=['tecplotPltReader'],
+    description='Interface for reading Tecplot binary files in Python',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Dark Light',
+    license='MIT',
+)
